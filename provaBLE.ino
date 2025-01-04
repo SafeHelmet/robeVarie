821089ce2a60d5aa95b30c1817ab05ee2b82b239
SIMONE MUSSINI <284900@studenti.unimore.it>
2025-01-04 15:14:54 +0100

    initial commit

---
+++ provaBLE.ino
@@ -0,0 +1,21 @@
+#include <ArduinoBLE.h>
+#include <string>
+#include <vector>
+
+const char * deviceServiceUuid = "f47ac10b-58cc-4372-a567-0e02b2c3d479";
+const char * deviceServiceRequestCharacteristicUuid = "f47ac10b-58cc-4372-a567-0e02b2c3d479";
+const char * deviceServiceResponseCharacteristicUuid = "f47ac10b-58cc-4372-a567-0e02b2c3d479
+";
+
+BLEService ledService(deviceServiceUuid);
+BLEStringCharacteristic
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
